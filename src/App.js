7565f4993da5d35702d05e5e4697c97774586e0d
SasiSekarP <109525298+SasiSekarP@users.noreply.github.com>
2023-03-09 12:06:32 +0530

    project over
    react useState is used

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import { FaStore } from 'react-icons/fa';
+import data from './data';
+import { useState } from 'react';
+
+function App() {
+  const { fetchedClothData, navBarData } = data;
+  const [clothData, setClothData] = useState(fetchedClothData);
+
+  const sortFn = (type) => {
+    if (type !== 'All') {
+      let newData = fetchedClothData.filter((singleData) => {
+        return (singleData.catagory === type);
+      })
+      
+      setClothData(newData);
+    } else {
+      setClothData(fetchedClothData);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className='heading-row'>
+      <FaStore className='logo' />
+      <h1 className='store-name'>Cloth Store</h1>
+      </div>
+      <div className='navBar'>
+        {navBarData.map((singleData,index) => {
+          return <div className='nav-btn' key={index} onClick={() => { sortFn(singleData)} }>{singleData}</div> 
+        })}
+      </div>
+      <div className='card-container'>
+        {clothData.map((singleData) => {
+          return <div className='card' key={singleData.id}>
+            <img src={singleData.image} className='photos' />
+            <div className='title'>{singleData.title}</div>
+            <p>{ singleData.discription }</p>
+            <div className='price'>₹ {singleData.price} only</div>
+            
+          </div> 
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,70 @@
+const navBarData = ['All','Shirt','Phant','T-shirt'];
+const fetchedClothData = [
+    {id:1,
+    title:'Zeroyaa',
+    catagory:'Shirt',
+    price:999,
+    image:'https://m.media-amazon.com/images/I/61eCByeioXL._UL1500_.jpg',
+    discription:"ZEROYAA Men's Long Sleeve Dress Shirt Solid Slim Fit Casual Business Formal Button Up Shirts with Pocket"
+    },
+    {id:2,
+    title:'Lyriq',
+    catagory:'Shirt',
+    price:599,
+    image:'https://m.media-amazon.com/images/I/41K8J6J3fUL.jpg',
+    discription: "Lyriq Casual Shirt for Men|| Shirt for Men|| Men Stylish Shirt || Men Printed Shirt"
+    },
+    {id:3,
+    title:'IndoPrimo',
+    catagory:'Shirt',
+    price:499,
+    image:'https://m.media-amazon.com/images/I/91vdgZmxtyL._UL1500_.jpg',
+    discription: "IndoPrimo Men's Cotton Casual Checkered Shirt for Men Full Sleeves"
+    },
+    {id:4,
+    title:'ADRO',
+    catagory:'T-shirt',
+    price:399,
+    image:'https://m.media-amazon.com/images/I/51TZSGQ63CL._UL1000_.jpg',
+    discription: "ADRO Men's Cotton Hooded Sweatshirt"
+    },
+    {id:5,
+    title:'Tfortees',
+    catagory:'T-shirt',
+    price:449,
+    image:'https://m.media-amazon.com/images/I/31k5wYcb0RL.jpg',
+    discription: 'Tfortees Mens Cotton Tshirt(AZ_1646_Black_)'
+    },
+    {id:6,
+    title:'Tfortees',
+    catagory:'T-shirt',
+    price:499,
+    image:'https://m.media-amazon.com/images/I/51yEWWjE9GL._UL1005_.jpg',
+    discription:"Tfortees Mens Cotton Tshirt(AZ_1748_White::Blue_)"
+    },
+    {id:7,
+    title:'Tfortees',
+    catagory:'T-shirt',
+    price:799,
+    image:'https://m.media-amazon.com/images/I/310nS751XmL.jpg',
+    discription: "Tfortees Mens Cotton Tshirt(AZ_1632_Black_)"
+    },
+    {id:8,
+    title:'FLYNOFF',
+    catagory:'Phant',
+    price:499,
+    image:'https://m.media-amazon.com/images/I/41rZh7pggML.jpg',
+    discription:'FLYNOFF Black Solid 4Way Lycra Tailored Fit Ankle Length Men\'s Track Pant'
+    },
+    {id:9,
+    title:'Symbol',
+    catagory:'Phant',
+    price:699,
+    image:'https://m.media-amazon.com/images/I/718kU7VISWL._UL1500_.jpg',
+    discription:"Amazon Brand - Symbol Men's Straight Casual Trousers"
+    }
+    ]
+
+const data = { fetchedClothData, navBarData };
+
+export default data;
